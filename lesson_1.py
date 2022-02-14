999cedf14cf294a11f1c5972a82389f96c95a9ae
amalmalik <amalmalik@mail.ru>
2022-02-14 17:59:08 +0500

    1st commit

---
+++ lesson_1.py
@@ -0,0 +1,29 @@
+# class Human:
+#     def _init__(self, full_name, age, job):
+#         self.name = full_name
+#         self.age = age
+#         self.job = job
+# human1 = Human('Amalmalik Abuzarov', 27, 'unknown')
+
+
+
+# class Animal:
+#     def __init__(self, full_name, age, breed):
+#         self.name = full_name
+#         self.age = age
+#         self.breed = breed
+
+
+# animal1 = Animal('Pretty', 3, 'no breed')
+
+# print(animal1.breed)
+
+# class Point: 
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+# point1 = Point(4, 1.5)
+# point2 = Point(-2, -2.5)
+# print(point1.x)
+
